95fb8e465c3bdbdf061fb5b38836f568351e2c82
kristijanGeorgiev <148566162+kristijanGeorgiev@users.noreply.github.com>
2024-10-02 00:41:40 +0200

    Delete Lecture 2 directory

--- Lecture 2/ext-script.js
+++
@@ -1,5 +0,0 @@
-console.log("This is an inline script");
-    var ptwo = document.getElementById("p-two");
-    var divone = document.getElementById("div-one");
-    console.log(`ptwo: ${ptwo.textContent}`);
-    console.log(`divone: ${divone.textContent}`);
