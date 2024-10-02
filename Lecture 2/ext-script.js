2e392a6a02863aaf06ed2b5c30f6a850528dae00
kristijanGeorgiev <148566162+kristijanGeorgiev@users.noreply.github.com>
2024-10-02 13:21:17 +0200

    Add files via upload

---
+++ Lecture 2/ext-script.js
@@ -0,0 +1,5 @@
+console.log("This is an inline script");
+    var ptwo = document.getElementById("p-two");
+    var divone = document.getElementById("div-one");
+    console.log(`ptwo: ${ptwo.textContent}`);
+    console.log(`divone: ${divone.textContent}`);
