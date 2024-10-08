43da76a0adfef0b2303344706bfb0cac1b957eab
kristijanGeorgiev <148566162+kristijanGeorgiev@users.noreply.github.com>
2024-10-08 19:01:04 +0200

    Delete Lecture 04 directory

--- Lecture 04/arrays.js
+++
@@ -1,3 +0,0 @@
-const numbers = [1,2,3,4]
-numbers.push(5)
-console.log(numbers)
--- Lecture 04/objects.js
+++
@@ -1,6 +0,0 @@
-const weko = {
-    firstName: 'Wekoslav',
-    lastName: 'Stefanovski',
-    age: 41
-}
-console.log(' the name is ' + weko.firstName + ' the surname is ' + weko.lastName + ' and the age is ' + weko.age)
