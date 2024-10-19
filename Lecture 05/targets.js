33482337f1ef551176a9a1d3b40ec2b7a25df20f
kristijanGeorgiev <148566162+kristijanGeorgiev@users.noreply.github.com>
2024-10-19 16:25:01 +0200

    Delete Lecture 05 directory

--- Lecture 05/targets.js
+++
@@ -1,9 +0,0 @@
-var data = [
-    ['Alice', 100],
-    ['David', 130],
-    ['Kristijan', 400],
-    ['Ljupka', 300],
-    ['Slobodanka', 130]
-];
-//const people: TPerson[] = data.map(([name, score]) =>({ name, score}));
-//const topScorer = people.reduce
